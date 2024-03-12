5cafaa3db3d50dd6c52e35732ea0c3ff4934cc81
rad5001 <rukundaime@gmail.com>
2024-03-12 20:29:08 +0200

    sort a list

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+
+const originList = Object.entries(dict);
+const val = Object.values(dict);                                            const valObj = [new Set(values)];
+const newDict = {}; 
+for (const i in valObj){
+	const list = []; 
+	for (const j in originl){ 
+		if(originl[j][1] === valObj[i]){
+			list.unshift(originl[j][0]);
+		}
+	} 
+	newDict[valObj[i]] = list;
+} 
+console.log(newDict);
