0bb1bd0ab92e3680349401ebb6ba2fadbecb568f
rad5001 <rukundaime@gmail.com>
2024-06-19 23:50:29 +0200

    fix space error

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,11 +4,10 @@
 - and finally displays the body of the response.
 """
 import sys
-import urllib.request
+import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    r = requests.get(url)
+    print(dict(request.headers.get("X-Request-Id")))
