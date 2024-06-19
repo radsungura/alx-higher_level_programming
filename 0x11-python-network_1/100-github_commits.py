931873f9e98039c923341508cc222d60f4f3edb5
rad5001 <rukundaime@gmail.com>
2024-06-19 21:53:41 +0200

    fix too long error

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""lists the 10 most recent commits on a given GitHub repository with Github API.
+"""lists the 10 most recent commits
+on a given GitHub repository with Github API.
 """
 import sys
 import requests
